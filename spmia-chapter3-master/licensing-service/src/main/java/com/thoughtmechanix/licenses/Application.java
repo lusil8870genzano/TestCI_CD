881216ae05e81d755e6a17200907d6c2ed8d0478
Luca Silvestri <N.Ambrosino.esterno@almaviva.it>
2023-09-28 15:10:35 +0200

    aggiunta fase di deploy 5

--- spmia-chapter3-master/licensing-service/src/main/java/com/thoughtmechanix/licenses/Application.java
+++ spmia-chapter3-master/licensing-service/src/main/java/com/thoughtmechanix/licenses/Application.java
@@ -1,12 +1,9 @@
 package com.thoughtmechanix.licenses;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
-@RefreshScope
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- spmia-chapter3-master/licensing-service/src/main/java/com/thoughtmechanix/licenses/controllers/LicenseServiceController.java
+++ spmia-chapter3-master/licensing-service/src/main/java/com/thoughtmechanix/licenses/controllers/LicenseServiceController.java
@@ -1,54 +1,17 @@
 package com.thoughtmechanix.licenses.controllers;
 
-import com.thoughtmechanix.licenses.model.License;
-import com.thoughtmechanix.licenses.services.LicenseService;
-import com.thoughtmechanix.licenses.config.ServiceConfig;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value="v1/organizations/{organizationId}/licenses")
+@RefreshScope
 public class LicenseServiceController {
-    @Autowired
-    private LicenseService licenseService;
+    @Value("${my.greeting}")
+    private String greetingMessage;
 
-    @Autowired
-    private ServiceConfig serviceConfig;
-
-    @RequestMapping(value="/",method = RequestMethod.GET)
-    public List<License> getLicenses( @PathVariable("organizationId") String organizationId) {
-
-        return licenseService.getLicensesByOrg(organizationId);
-    }
-
-    @RequestMapping(value="/{licenseId}",method = RequestMethod.GET)
-    public License getLicenses( @PathVariable("organizationId") String organizationId,
-                                @PathVariable("licenseId") String licenseId) {
-
-        return licenseService.getLicense(organizationId,licenseId);
-    }
-
-    @RequestMapping(value="{licenseId}",method = RequestMethod.PUT)
-    public String updateLicenses( @PathVariable("licenseId") String licenseId) {
-        return String.format("This is the put");
-    }
-
-    @RequestMapping(value="/",method = RequestMethod.POST)
-    public void saveLicenses(@RequestBody License license) {
-       licenseService.saveLicense(license);
-    }
-
-    @RequestMapping(value="{licenseId}",method = RequestMethod.DELETE)
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public String deleteLicenses( @PathVariable("licenseId") String licenseId) {
-        return String.format("This is the Delete");
+    @GetMapping("/greeting")
+    public String greeting(){
+        return "my.greeting: " + greetingMessage ;
     }
 }
